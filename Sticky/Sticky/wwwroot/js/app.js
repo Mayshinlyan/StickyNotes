64d2d14fd6a4d1112a431e0857bd9529a24c1221
Mayshinlyan <hwai@u.rochester.edu>
2018-11-08 17:39:34 -0500

    update

--- Sticky/Sticky/wwwroot/js/app.js
+++ Sticky/Sticky/wwwroot/js/app.js
@@ -10,7 +10,8 @@ function createNote(eventX;
 $(function(){
     $(".flex").click(function(e){
         if($(e.target).is("header")) return;
-        if($(e.target).is("textarea")) return;
+        if ($(e.target).is("textarea")) return;
+
         if($(e.target).is("div")) return;
         if($(e.target).is("h1")) return;
         if(e.pageY > (window.innerHeight-202)){e.pageY=window.innerHeight-202;}
@@ -19,7 +20,7 @@ $(function(){
                 "left": e.pageX + 'px',
                 "top": e.pageY + 'px'
             })
-            .append($('<div class="stickynote"><header class="ui-widget-content"></header><body><textarea class="stickyForm"></textarea></div>'))
+            .append($('<div class="stickynote"><header class="ui-widget-content"></header><body><textarea class="stickyForm" id="inputText"></textarea></div>'))
             .appendTo(document.body);
             $( ".stickynote" ).draggable({handle:"header", containment:"#board"});
     });
--- Sticky/Sticky/wwwroot/js/note.js
+++ Sticky/Sticky/wwwroot/js/note.js
@@ -4,7 +4,7 @@ var connection = new signalR.HubConnectionBuilder().withUrl("/noteHub").build();
 
 connection.on("ReceiveMessage", function (user, message) {
     var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
-    var input = document.getElementById("messageInput");
+    var input = document.getElementById("inputText");
     input.value = msg;
 
 });
@@ -13,9 +13,9 @@ connection.start().catch(function (err) {
     return console.error(err.toString());
 });
 
-document.getElementById("messageInput").addEventListener("input", function (event) {
-    var user = document.getElementById("userInput").value;
-    var message = document.getElementById("messageInput").value;
+document.getElementById("inputText").addEventListener("input", function (event) {
+   // var user = document.getElementById("userInput").value;
+    var message = document.getElementById("inputText").value;
     connection.invoke("SendMessage", user, message).catch(function (err) {
         return console.error(err.toString());
     });
