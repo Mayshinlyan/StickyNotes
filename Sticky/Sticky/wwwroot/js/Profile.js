ee30a4f8683b411677b937868d09f20ab1b9cd4c
ajarvis3 <33203802+ajarvis3@users.noreply.github.com>
2018-12-20 10:40:37 -0500

    Add a comment

--- Sticky/Sticky/wwwroot/js/Profile.js
+++ Sticky/Sticky/wwwroot/js/Profile.js
@@ -1,4 +1,9 @@
-﻿function goToBoard(id) {
+﻿/**
+ * 
+ * Redirects to a board from the profile
+ * @param {any} id the id of the board to go to
+ */
+function goToBoard(id) {
     localStorage.setItem("board", id);
     let url = window.location.href.replace(window.location.pathname, '');
     url = url + "/Board";
