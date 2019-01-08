bed6644bb000df5658b1d97a55e7c3feca9b58ea
ajarvis3 <33203802+ajarvis3@users.noreply.github.com>
2019-01-07 21:39:51 -0500

    Revert "Revert "fixed button""
    This reverts commit 06c23dbbdd853c47be6e8699df557464007e87d0.

---
+++ Sticky/Sticky/wwwroot/js/app.js
@@ -0,0 +1,11 @@
+/*
+var currentNote = {};
+function createNote(eventX;
+    var y = event.clientY;
+    currentNote.position = $("x","y");
+    document.getElementById("demo").innerHTML = coords1 + coords2;
+}
+*/
+
+
+
